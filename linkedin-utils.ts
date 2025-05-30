7e34f88fd375597a45b1cd69e783e2ccc660c838
Matt Dailey <matthew.j.dailey@gmail.com>
2025-05-30 13:59:54 -0700

    Clean up some duplicate code

--- linkedin-utils.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from 'puppeteer'
-import { scrollAndGatherLinkedin } from './social-media-utils.js'
-
-// Re-export the LinkedIn function
-export { scrollAndGatherLinkedin }
--- twitter-utils.ts
+++
@@ -1,5 +0,0 @@
-import { Page } from 'puppeteer'
-import { scrollAndGatherTwitter } from './social-media-utils.js'
-
-// Re-export the refactored function for backward compatibility
-export { scrollAndGatherTwitter }
